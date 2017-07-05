bef9d6a43261b71d9f9c084a6d44274aab23a210
marshalldanel <marshalldanel@gmail.com>
2017-07-05 01:26:15 +0000

    URL submission form with POST requests and request routing

--- express_server.js
+++ express_server.js
@@ -1,8 +1,11 @@
-var express = require('express');
-var app = express();
-var PORT = process.env.PORT || 8080;
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const PORT = process.env.PORT || 8080;
 
 app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 var urlDatabase = {
   'b2xVn2': 'http://www.lighthouse.cs',
@@ -13,19 +16,27 @@ app.get('/', (req, res) => {
   res.end('Hello!');
 });
 
+app.get('/hello', (req, res) => {
+  res.end('<html><body>Hello <b>World</b></body></html>\n');
+});
+
+app.get('/urls/new', (req, res) => {
+  res.render('urls_new');
+});
+
+app.post('/urls', (req, res) => {
+  console.log(req.body);
+  res.send('OK');
+});
+
 app.get('/urls', (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render('urls_index', templateVars);
 });
 
 app.get('/urls/:id', (req, res) => {
-  
   let templateVars = { shortURL: req.params.id, longURL: urlDatabase[req.params.id]};
   res.render('urls_show', templateVars);
-});
-
-app.get('/hello', (req, res) => {
-  res.end('<html><body>Hello <b>World</b></body></html>\n');
 });
 
 app.listen(PORT, () => {
